e2ed327193c5753af4e6988fa022a5610a8c850a
IMAC <imac@IMACs-iMac.local>
2021-10-25 17:14:31 +0400

    initial commit

---
+++ myLecture27/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myLecture27
+//
+//  Created by IMAC on 25.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
