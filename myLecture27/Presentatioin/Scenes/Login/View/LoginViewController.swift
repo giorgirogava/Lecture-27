542a918b8249ce66ec74913023acb343661f06cf
giorgirogava <41551187+giorgirogava@users.noreply.github.com>
2021-10-25 19:30:08 +0400

    Feature/login flow (#1)
    * - add login page with basic UI components
    
    * - improved ui login page

---
+++ myLecture27/Presentatioin/Scenes/Login/View/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  myLecture27
+//
+//  Created by IMAC on 25.10.21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
