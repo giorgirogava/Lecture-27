ca04eff46c9253660d1bcbdee97f1275aa9f5b20
giorgirogava <giorgirogava01ai@gmail.com>
2021-10-25 18:40:57 +0400

    - add infrastructure layer
    - add button extension

---
+++ myLecture27/Infrastructure/Extensions/UIButton+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIButton+Extension.swift
+//  myLecture27
+//
+//  Created by IMAC on 25.10.21.
+//
+
+import UIKit
+
+extension UIButton {
+    func setBackgroundColor(color: UIColor) {
+        backgroundColor = color
+    }
+}
